a2266d09377f258c0af72e43a1a6ce8704e06241
ayush1631 <ayushmaurya1631@gmail.com>
2024-06-30 09:57:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,8 +18,8 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'TechVJBot')
-API_ID = int(environ.get('API_ID', '7284429084'))
-API_HASH = environ.get('API_HASH', '7331873035:AAFeUHX0gBgzHC1Wj71zr7LwFfYXkyKtJ-o')
+API_ID = int(environ.get('API_ID', '29692684'))
+API_HASH = environ.get('API_HASH', '35110e9538c2c1c7fbe7ea4e84b4b8c0')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Bot settings
